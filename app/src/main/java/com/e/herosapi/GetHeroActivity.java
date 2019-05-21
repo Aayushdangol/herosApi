6f101d05616360cb3d77365354505a1bbc0009e5
Softwarica College Lab <softwarica@softwarica.com>
2019-05-21 11:40:30 +0545

    post and get using field

--- app/src/main/java/com/e/herosapi/GetHeroActivity.java
+++ app/src/main/java/com/e/herosapi/GetHeroActivity.java
@@ -2,12 +2,64 @@ package com.e.herosapi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import ClientApi.HeroesApi;
+import model.Heroes;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import url.Url;
 
 public class GetHeroActivity extends AppCompatActivity {
+
+    private TextView tvdata;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_hero);
+
+        tvdata = findViewById(R.id.tvData);
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(Url.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create()).build();
+
+        HeroesApi heroesApi = retrofit.create(HeroesApi.class);
+
+        Call<List<Heroes>> heroCall = heroesApi.getHeroes();
+
+        heroCall.enqueue(new Callback<List<Heroes>>() {
+            @Override
+            public void onResponse(Call<List<Heroes>> call, Response<List<Heroes>> response) {
+                if (!response.isSuccessful()){
+                    tvdata.setText("Code: "+ response.code());
+                    return;
+                }
+
+                List<Heroes> heroesList = response.body();
+                for (Heroes heroes: heroesList ){
+
+                    String content = "";
+                    content += "ID: " + heroes.getId() + "\n";
+                    content += "Name: "+heroes.getName() + "\n";
+                    content += "ID: " + heroes.getDesc() + "\n";
+
+
+                tvdata.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Heroes>> call, Throwable t) {
+                tvdata.setText("Error " + t.getMessage());
+            }
+        });
     }
 }
--- app/src/main/java/model/Heroes.java
+++ app/src/main/java/model/Heroes.java
@@ -2,25 +2,25 @@ package model;
 
 public class Heroes {
 
-//    private String id;
+    private String id;
     private String name;
     private String desc;
 //    private String image;
 
-    public Heroes( String name, String desc) {
-//        this.id = id;
+    public Heroes(String id, String name, String desc) {
+        this.id = id;
         this.name = name;
         this.desc = desc;
 //        this.image = image;
     }
 
-//    public String getId() {
-//        return id;
-//    }
-//
-//    public void setId(String id) {
-//        this.id = id;
-//    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
